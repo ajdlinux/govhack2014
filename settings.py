60f0cd3707375b318f087ae8f5d8af2188ff6fc2
Andrew Donnellan <andrew@donnellan.id.au>
2014-07-12 10:51:43 +1000

    settings.py

---
+++ settings.py
@@ -0,0 +1,23 @@
+"""
+Where should I? - a tool to help you find where to do things!
+Copyright © 2014 Andrew Donnellan, Teresa Bradbury, Suzette Bailey, Craig Thomler.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+# settings.py - local settings
+
+SERVER_HOSTNAME = "whereshouldi.donnellan.id.au"
+SERVER_HOST = "0.0.0.0"
+SERVER_PORT = 8083
