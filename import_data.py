389d1014481893911cdc351c6493c3cdfbaca315
Tessa Bradbury <tessereth@yahoo.com.au>
2014-07-12 22:43:40 +1000

    use csv library for less hacky parsing

--- import_data.py
+++ import_data.py
@@ -1,12 +1,14 @@
 from db import *
+import csv
 
 def import_schools(db, filename):
-    f = open(filename, 'r')
+    f = open(filename, 'rb')
     cur = db.cursor()
-    f.readline() # consume header
-    for line in f:
-        cols = line.split(',')
-        cur.execute("insert into schools (name, address, Suburb, Sector, location) values (%s, %s, %s, %s, ST_PointFromText(%s))", cols)
+    #f.readline() # consume header
+    for line in csv.reader(f):
+        print line
+        cur.execute("insert into schools (name, address, Suburb, Sector, location) values (%s, %s, %s, %s, ST_PointFromText(%s))", line)
+    f.close()
 
 db = DB()
 import_schools(db, "~/ACT_School_Locations.csv")
