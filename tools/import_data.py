604e38e2f0a5f431f19d9f343e1c4f80af8e2355
Tessa Bradbury <tessereth@yahoo.com.au>
2014-07-12 22:32:31 +1000

    bugs

--- tools/import_data.py
+++ tools/import_data.py
@@ -3,9 +3,10 @@ from db import *
 def import_schools(db, filename):
     f = open(filename, 'r')
     cur = db.cursor()
+    f.readline() # consume header
     for line in f:
         cols = line.split(',')
-        cur.execute("insert into schools values (%s, %s, %s, %s, ST_PointFromText(%s))", cols)
+        cur.execute("insert into schools (name, address, Suburb, Sector, location) values (%s, %s, %s, %s, ST_PointFromText(%s))", cols)
 
 db = DB()
 import_schools(db, "~/ACT_School_Locations.csv")
