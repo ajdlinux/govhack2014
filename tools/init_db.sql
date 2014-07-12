835ba705cd84844da163c36cac8f86199e6e7e79
Andrew Donnellan <andrew@donnellan.id.au>
2014-07-12 14:59:27 +1000

    maybe with quotes it will work?

--- tools/init_db.sql
+++ tools/init_db.sql
@@ -1,4 +1,4 @@
-CREATE USER whereshouldi WITH PASSWORD whereshouldi;
+CREATE USER whereshouldi WITH PASSWORD 'whereshouldi';
 CREATE DATABASE whereshouldi;
 GRANT ALL PRIVILEGES ON DATABASE whereshouldi TO whereshouldi;
 
