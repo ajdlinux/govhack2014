787cb9d19f65f5317d6de9cbb7fc4c2c8b790ec6
Andrew Donnellan <andrew@donnellan.id.au>
2014-07-12 10:51:36 +1000

    main.py

---
+++ main.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+"""
+Where should I? - a tool to help you find where to do things!
+Copyright © 2014 Andrew Donnellan, Teresa Bradbury, Suzette Bailey, Craig Thomler.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+# main.py - main Bottle script
+
+import sys
+import os
+
+import bottle
+
+import settings
+
+@bottle.route('/')
+@bottle.route('/index.html')
+@bottle.view('index')
+def index():
+    return {'hostname': settings.SERVER_HOSTNAME}
+
+@bottle.route('/about')
+@bottle.view('about')
+def about():
+    return {}
+
+@bottle.route('/contact')
+@bottle.view('contact')
+def contact():
+    return {}
+
+
+if __name__ == "__main__":
+    # Make sure we're in the right directory!
+    if os.path.dirname(__file__):
+        os.chdir(os.path.dirname(__file__))
+        sys.path.append(os.path.dirname(__file__))
+
+    # Lights! Camera! ACTION!
+    bottle.run(reloader=True, debug=True, host=settings.SERVER_HOST, port=settings.SERVER_PORT)
