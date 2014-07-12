bd85fe486289d1662429f51b4b1b7a17589fecd0
Tessa Bradbury <tessereth@yahoo.com.au>
2014-07-12 15:02:32 +1000

    ads.stat helper functions

---
+++ abs_stat.py
@@ -0,0 +1,28 @@
+import urllib
+import urllib2
+import json
+
+def abs_get(get_dict):
+    base = "http://stat.abs.gov.au/itt/query.jsp?"
+    get_dict['format'] = 'json'
+    args = urllib.urlencode(get_dict)
+    f = urllib2.urlopen(base + args)
+    data = json.load(f)
+    f.close()
+    return data
+
+def get_concepts(datasetid):
+    return abs_get({'method' : 'GetDatasetConcepts', 'datasetid' : datasetid})['concepts']
+
+def get_codes(datasetid, concept):
+    return abs_get({'method' : 'GetCodeListValue', 'datasetid' : datasetid, 'concept' : concept})['codes']
+
+def get_all_codes(datasetid):
+    concepts = get_concepts(datasetid)
+    codes = {}
+    for concept in concepts:
+        codes[concept] = get_codes(datasetid, concept)
+    return codes
+
+def pretty_print(data):
+    print json.dumps(data, sort_keys=True, indent=4, separators=(',', ': '))
