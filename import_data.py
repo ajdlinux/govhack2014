e6b581d087ff226430e9f9d363e8a78d41aada75
Tessa Bradbury <tessereth@yahoo.com.au>
2014-07-12 23:06:46 +1000

    now with a transaction

--- import_data.py
+++ import_data.py
@@ -5,6 +5,7 @@ def import_schools(db, filename):
     f = open(filename, 'rb')
     cur = db.cursor()
     f.readline() # consume header
+    cur.execute("begin")
     for line in csv.reader(f):
         print line
         coord = [float(p) for p in line[-1].strip('"').strip('(').strip(')').split(', ')]
@@ -12,7 +13,8 @@ def import_schools(db, filename):
         line[-1] = "(%f, %f)" % tuple(coord)
         cur.execute("insert into schools (name, address, Suburb, Sector, location) values (%s, %s, %s, %s, %s)", line)
     f.close()
+    cur.execute("commit")
 
 db = DB()
 import_schools(db, "ACT_School_Locations.csv")
-db.close()
+db.disconnect()
