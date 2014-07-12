16b4a5702eb001d221a04cc61c71de4c79d40fa1
Andrew Donnellan <andrew@donnellan.id.au>
2014-07-12 13:34:29 +1000

    moar infrastructure

--- fabfile.py
+++ fabfile.py
@@ -21,6 +21,7 @@ def setup():
 
 def init_ec2():
     run("sudo apt-get update; sudo apt-get upgrade -y")
+    run("sudo apt-get install -y supervisor nginx apache2-")
     run("sudo apt-get install -y python-virtualenv python-dev postgresql-9.3 postgresql-client-9.3 postgresql-contrib-9.3 libpq-dev git fabric phppgadmin libxml2-dev libxslt1-dev python-pip")
     run("[ -e /home/wsi ] || sudo useradd -m wsi")
     run("sudo rm -rf /home/wsi/whereshouldi")
@@ -28,14 +29,15 @@ def init_ec2():
     run("cd /home/wsi/whereshouldi; sudo fab setup")
     #run("cd /home/wsi/whereshouldi; sudo tools/setup_wsi.py")
     run("""cat > /tmp/wsi.conf << __EOF__
-    [program:wsi]
+[program:wsi]
 user=wsi
 command=/home/wsi/whereshouldi/main.py
 autostart=true
 autorestart=true
 __EOF__""")
     run("sudo mv /tmp/wsi.conf /etc/supervisor/conf.d/")
-    run("sudo supervisorctl start wsi")
+    run("sudo supervisorctl reread")
+    run("sudo supervisorctl reload") #start wsi")
     run("""cat > /tmp/default << __EOF__
 upstream wsi {
         server 127.0.0.1:8080;
