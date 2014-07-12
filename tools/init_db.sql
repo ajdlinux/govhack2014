df7236344d93af69ae93e98a1391041a9339b3ff
Andrew Donnellan <andrew@donnellan.id.au>
2014-07-12 14:55:14 +1000

    init database sql script

---
+++ tools/init_db.sql
@@ -0,0 +1,7 @@
+CREATE USER whereshouldi WITH PASSWORD whereshouldi;
+CREATE DATABASE whereshouldi;
+GRANT ALL PRIVILEGES ON DATABASE whereshouldi TO whereshouldi;
+
+CREATE EXTENSION cube;
+CREATE EXTENSION earthdistance;
+CREATE EXTENSION postgis;
