328e79ffcb0633777f62f608fa8d5d6f967e51bb
Tessa Bradbury <tessereth@yahoo.com.au>
2014-07-13 12:57:22 +1000

    add hospital data

--- tools/init_db.sql
+++ tools/init_db.sql
@@ -13,6 +13,17 @@ CREATE TABLE schools (
     location point
 );
 
+create table hospitals (id serial primary key, name text, location geometry(point));
+
 CREATE FUNCTION school_distance(geometry) RETURNS float as $$
-    select ST_Distance(location, $1) as d from schools order by d asc limit 1;
+    select ST_Distance(location, ST_Centroid($1)) as d from schools order by d asc limit 1;
 $$ LANGUAGE SQL;
+
+CREATE FUNCTION hospital_distance(geometry) RETURNS float as $$
+    select ST_Distance(location, ST_Centroid($1)) as d from hospitals order by d asc limit 1;
+$$ LANGUAGE SQL;
+
+insert into hospitals (name, location) values
+    ('Canberra Hospital & Health Services', 'POINT(149.100637 -35.345912)'),
+    ('Calvary Public Hospital', 'POINT(149.086731 -35.253254)'),
+    ('QEII Family Centre', 'POINT(149.079181 -35.323296)');
