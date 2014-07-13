d6bb0406f662a489b73bd8dad1debb98f52247bf
Tessa Bradbury <tessereth@yahoo.com.au>
2014-07-13 13:17:43 +1000

    copy paste error

--- tools/boundary.sh
+++ tools/boundary.sh
@@ -7,4 +7,4 @@ psql postgresql://whereshouldi@localhost -f sa2.sql
 psql postgresql://whereshouldi@localhost -c "alter table sa2 add column school_distance float;"
 psql postgresql://whereshouldi@localhost -c "update sa2 set school_distance = school_distance(geom);"
 psql postgresql://whereshouldi@localhost -c "alter table sa2 add column hospital_distance float;"
-psql postgresql://whereshouldi@localhost -c "update sa2 set school_distance = hospital_distance(geom);"
+psql postgresql://whereshouldi@localhost -c "update sa2 set hospital_distance = hospital_distance(geom);"
